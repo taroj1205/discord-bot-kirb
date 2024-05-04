e5cd2632909621287fa0e24cac21cf121a12c9a6
taroj1205 <taroj1205@gmail.com>
2024-05-05 09:57:40 +1200

    feat: add nz command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,20 +2,22 @@ import * as ping from "./ping";
 import * as github from "./github";
 import * as kirb from "./kirb";
 import * as dice from "./dice";
-import * as messages from "./config/random-messages"
+import * as messages from "./config/random-messages";
 import * as leave from "./leave";
 import * as invite from "./invite";
 import * as common from "./common";
 import * as timestamp from "./timestamp";
+import * as nz from "./new-zealand";
 
 export const commands = {
-  ping,
-  github,
-  kirb,
-  dice,
-  config: messages,
-  leave,
-  invite,
-  common,
-  timestamp
+	ping,
+	github,
+	kirb,
+	dice,
+	config: messages,
+	leave,
+	invite,
+	common,
+	timestamp,
+	nz,
 };
--- src/index.ts
+++ src/index.ts
@@ -5,58 +5,75 @@ import { deployCommands } from "./deploy-commands";
 import { get } from "./sql";
 
 const client = new Client({
-  intents: ["Guilds", "GuildMessages", "DirectMessages"],
+	intents: ["Guilds", "GuildMessages", "DirectMessages"],
 });
 
 client.once("ready", () => {
-  console.log("Discord bot is ready! 🤖");
+	console.log("Discord bot is ready! 🤖");
 });
 
 client.on("guildCreate", async (guild) => {
-  await deployCommands({ guildId: guild.id });
+	await deployCommands({ guildId: guild.id });
 });
 
 client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) {
-    return;
-  }
+	try {
+		if (!interaction.isCommand()) {
+			return;
+		}
 
-  const { commandName } = interaction;
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  const timestamp = `${hours}:${minutes}:${seconds}`;
+		const { commandName } = interaction;
+		const now = new Date();
+		const hours = now.getHours().toString().padStart(2, "0");
+		const minutes = now.getMinutes().toString().padStart(2, "0");
+		const seconds = now.getSeconds().toString().padStart(2, "0");
+		const timestamp = `${hours}:${minutes}:${seconds}`;
 
-  console.log(`[${timestamp}] Running command ${commandName}`);
-  if (commands[commandName as keyof typeof commands]) {
-    commands[commandName as keyof typeof commands].execute(interaction);
-  }
+		console.log(`[${timestamp}] Running command ${commandName}`);
+		if (commands[commandName as keyof typeof commands]) {
+			commands[commandName as keyof typeof commands].execute(interaction);
+			console.log(`[${timestamp}] Finished command ${commandName}`);
+		}
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 client.on("messageCreate", async (message) => {
-  // Ignore messages from bots and kirb
-  if (message.author.bot || message.author.id === '765061967961784321') return;
+	// Ignore messages from bots and kirb
+	if (message.author.bot || message.author.id === "765061967961784321") return;
 
-  const { messages: words, chance, channels } = await get(message.guild?.id!) || { messages: "L", chance: 1, channels: null };
+	const {
+		messages: words,
+		chance,
+		channels,
+	} = (await get(message.guild?.id!)) || {
+		messages: "L",
+		chance: 1,
+		channels: null,
+	};
 
-  // 1% chance of being triggered when chance is 1
-  if (Math.random() < chance / 100) {
-    if (channels && channels[message.channelId] === true) {
-      // Pick random word from the list
-      const word = words[Math.floor(Math.random() * words.length)];
-      await message.channel.send(word);
-    }
-  }
+	// 1% chance of being triggered when chance is 1
+	if (Math.random() < chance / 100) {
+		if (channels && channels[message.channelId] === true) {
+			// Pick random word from the list
+			const word = words[Math.floor(Math.random() * words.length)];
+			await message.channel.send(word);
+		}
+	}
 
-  // If the user is 631578250144907269 and the message mentions the bot and includes "leave"
-  if (message.author.id === '631578250144907269' && message.mentions.has(client.user!) && message.content.toLowerCase().includes('leave')) {
-    // Leave the server
-    if (message.guild) {
-      await message.reply("Fine! I'm leaving");
-      await message.guild.leave();
-    }
-  }
+	// If the user is 631578250144907269 and the message mentions the bot and includes "leave"
+	if (
+		message.author.id === "631578250144907269" &&
+		message.mentions.has(client.user!) &&
+		message.content.toLowerCase().includes("leave")
+	) {
+		// Leave the server
+		if (message.guild) {
+			await message.reply("Fine! I'm leaving");
+			await message.guild.leave();
+		}
+	}
 });
 
 client.login(config.DISCORD_TOKEN);
